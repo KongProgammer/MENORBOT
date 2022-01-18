49c97426362c57cfe455cf8430f2c263613f520a
KongProgammer <81782546+KongProgammer@users.noreply.github.com>
2022-01-18 14:57:34 -0300

    Update daily.js

--- src/commands/economy/daily.js
+++ src/commands/economy/daily.js
@@ -1,6 +1,6 @@
 const db = require('quick.db');
 const ms = require('parse-ms');
-
+const Discord = require('discord.js')
 exports.run = async (bot, message, args) => {
         let user = message.author;
         let timeout = 86400000;
